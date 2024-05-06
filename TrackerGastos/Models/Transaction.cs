f316cb31b12a5701c90524ed0317ad644b14fcd9
SantiagoSauma <lssaumap@gmail.com>
2024-05-05 21:34:30 -0600

    Agregar archivos de proyecto.

---
+++ TrackerGastos/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrackerGastos.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
