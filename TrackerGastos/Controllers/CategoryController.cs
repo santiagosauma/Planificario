d8b4f2796293d6d5953a1d8e9ba240132c777e62
SantiagoSauma <lssaumap@gmail.com>
2024-05-06 02:06:59 -0600

    Updating styles and managing syncfusion

--- TrackerGastos/Controllers/CategoryController.cs
+++ TrackerGastos/Controllers/CategoryController.cs
@@ -45,7 +45,7 @@ namespace TrackerGastos.Controllers
         // GET: Category/Create
         public IActionResult Create()
         {
-            return View();
+            return View(new Category());
         }
 
         // POST: Category/Create
